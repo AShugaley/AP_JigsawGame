835729ea146cbf00df7b40acefde8ceccc785eac
ofri64 <ofri64@gmail.com>
2018-01-16 12:27:57 -0800

    added ex4headers.h header file like moav asked

---
+++ hw4/ex4headers.h
@@ -0,0 +1,12 @@
+//
+// Created by okleinfeld on 1/16/18.
+//
+
+#ifndef AP_JIGSAWGAME_EX4HEADERS_H
+#define AP_JIGSAWGAME_EX4HEADERS_H
+
+#include "Puzzle2dPiece.h"
+#include "Puzzle3dPiece.h"
+#include "PuzzleGroups.h"
+
+#endif //AP_JIGSAWGAME_EX4HEADERS_H
--- hw4/main.cpp
+++ hw4/main.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 #include <list>
 #include <vector>
-#include "Puzzle2dPiece.h"
-#include "Puzzle3dPiece.h"
-#include "PuzzleGroups.h"
+#include "ex4headers.h"
 
 using namespace std;
 
 int main() {
-//    list<Puzzle2dPiece<5>> pieces = {{0, 3, 2, -5}, {0, -2, 2, -5}};
-//    auto groups = groupPuzzlePieces(pieces.begin(), pieces.end());
-//    auto some_pieces = groups.get({0, 3, 2, numeric_limits<int>::min()});
-//    for (auto piece_ptr : some_pieces){
-//        cout << *piece_ptr << endl;
-//    }
-    auto joker = numeric_limits<int>::min();
-    list<Puzzle3dPiece<1>> pieces = {{0, 1, 1, 1, -1, -1}, {0, -1, 1, 1, 1, 1}};
+    list<Puzzle2dPiece<5>> pieces = {{0, 3, 2, -5}, {0, -2, 2, -5}};
     auto groups = groupPuzzlePieces(pieces.begin(), pieces.end());
-    auto some_pieces = groups.get({0, joker, 1, 1, joker, joker});
+    auto some_pieces = groups.get({0, numeric_limits<int>::min(), 2, numeric_limits<int>::min()});
     for (auto piece_ptr : some_pieces){
         cout << *piece_ptr << endl;
     }
+//    auto joker = numeric_limits<int>::min();
+//    list<Puzzle3dPiece<1>> pieces = {{0, 1, 1, 1, -1, -1}, {0, -1, 1, 1, 1, 1}};
+//    auto groups = groupPuzzlePieces(pieces.begin(), pieces.end());
+//    auto some_pieces = groups.get({0, joker, 1, 1, joker, joker});
+//    for (auto piece_ptr : some_pieces){
+//        cout << *piece_ptr << endl;
+//    }
     return 0;
 }
